8c4c045ad4d41f179ca0d02dafc7c2d3d40a7c44
Matthew Horne Negro <matthew@quimify.com>
2023-10-19 13:18:52 +0200

    Created 4 new classes (Game.java, Labyrinth.java, Player.java and Monster.java). Monster.java also implemented fully.

---
+++ Monster.java
@@ -0,0 +1,40 @@
+public class Monster {
+    private final static int INITIAL_HEALTH = 5;
+    private final String name;
+    private final float intelligence;
+    private final float strength;
+    private float health;
+    private int row;
+    private int col;
+
+    public Monster(String name, float intelligence, float strength) {
+        this.name = name;
+        this.intelligence = intelligence;
+        this.strength = strength;
+    }
+
+    public boolean dead() {
+        return health == 0;
+    }
+
+    public float attack() {
+        return (new Dice()).intensity(strength);
+    }
+
+    public void setPos(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public String toString() {
+        return "Name: " + name + "Intelligence: " + intelligence + "Strentgh: " + strength + "Health: " + health;
+    }
+
+    private void gotWounded() {
+        health--;
+    }
+
+    public boolean defend(float receivedAttack) {
+        throw new UnsupportedOperationException();
+    }
+}
