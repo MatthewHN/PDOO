6fe19eb50d8b154a289b4fff2353b58b76866974
Matthew Horne Negro <matthew@quimify.com>
2023-11-15 16:25:31 +0100

    Player.java implemented as asked in project guide 2

--- Dice.java
+++ Dice.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Dice {
 
-    private static final Random generator = new Random();
+    private static Random generator = new Random();
     private static final int MAX_USES = 5;
     private static final float MAX_INTELLIGENCE = 10.0F;
     private static final float MAX_STRENGTH = 10.0F;
@@ -18,7 +18,7 @@ public class Dice {
         return generator.nextFloat() < probability;
     }
 
-    public static float intensity(float competence){
+    public static float intensity(float competence) {
         return generator.nextFloat(competence);
     }
 
@@ -42,27 +42,27 @@ public class Dice {
         return generator.nextFloat() <= RESURRECT_PROB;
     }
 
-    public static int weaponsReward(){
+    public static int weaponsReward() {
         return generator.nextInt(WEAPONS_REWARD);
     }
 
-    public static int shieldsReward(){
+    public static int shieldsReward() {
         return generator.nextInt(SHIELDS_REWARD);
     }
 
-    public static int healthReward(){
+    public static int healthReward() {
         return generator.nextInt(HEALTH_REWARD);
     }
 
-    public static float weaponPower(){
+    public static float weaponPower() {
         return generator.nextFloat(MAX_ATTACK);
     }
 
-    public static float shieldPower(){
+    public static float shieldPower() {
         return generator.nextFloat(MAX_SHIELD);
     }
 
-    public static int usesLeft(){
+    public static int usesLeft() {
         return generator.nextInt(MAX_USES);
     }
 
--- Player.java
+++ Player.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Player {
     private static final int MAX_WEAPONS = 2;
     private static final int MAX_SHIELDS = 3;
@@ -12,101 +14,146 @@ public class Player {
     private int col;
     private int consecutiveHits = 0;
 
+    private ArrayList<Weapon> weapons;
+    private ArrayList<Shield> shields;
 
-    public Player (char number, float intelligence, float strength){
+
+    public Player(char number, float intelligence, float strength) {
         this.number = number;
         this.intelligence = intelligence;
         this.strength = strength;
+        this.health = INITIAL_HEALTH;
+        weapons = new ArrayList<>(MAX_WEAPONS);
+        shields = new ArrayList<>(MAX_SHIELDS);
+        row = 0;
+        col = 0;
+        this.name = "Player # " + number;
     }
 
-    public void resurrect(){
-
+    public void resurrect() {
+        this.weapons.clear(); // Hace que la lista de armas sea una lista vacía
+        this.shields.clear(); // Hace que la lista de escudos sea una lista vacía
+        this.health = INITIAL_HEALTH; // Restablece la salud al nivel inicial
+        resetHits(); // Restablece el número de golpes consecutivos a cero
     }
 
-    public int getRow(){
-
+    public int getRow() {
+        return this.row;
     }
 
-    public int getCol(){
-
+    public int getCol() {
+        return this.col;
     }
 
-    public char getNumber(){
-
+    public char getNumber() {
+        return this.number;
     }
 
-    public void setPos(){int row, int col}{
+    public void setPos(int row, int col) {
         this.row = row;
         this.col = col;
 
     }
 
-    public boolean dead(){
-
+    public boolean dead() {
+        return this.health <= 0;
     }
 
-    public Directions move (Directions direction, Directions[] validMoves){
-
+    public Directions move(Directions direction, Directions[] validMoves) {
+        throw new UnsupportedOperationException();
     }
 
-    public float attack(){
-
+    public float attack() {
+        // Sumar la fuerza del jugador y el total aportado por sus armas
+        return this.strength + sumWeapons();
     }
 
-    public boolean defend (float receivedAttack){
-
+    public boolean defend(float receivedAttack) {
+        return manageHit(receivedAttack);
     }
 
-    public void receiveReward(){
-
+    public void receiveReward() {
+        throw new UnsupportedOperationException();
     }
 
-    public String toString(){
+    public String toString() {
 
+        return "Player{name='" + name +
+                "', number=" + number +
+                ", intelligence=" + intelligence +
+                ", strength=" + strength +
+                ", health=" + health +
+                ", row=" + row +
+                ", col=" + col +
+                ", consecutiveHits=" + consecutiveHits +
+                ", Weapons=" + weapons.toString() +
+                ", Shields=" + shields.toString() +
+                "}";
     }
 
-    private void receiveWeapon(Weapon w){
-
+    private void receiveWeapon(Weapon w) {
+        throw new UnsupportedOperationException();
     }
 
-    private void receiveShield(Shield s){
-
+    private void receiveShield(Shield s) {
+        throw new UnsupportedOperationException();
     }
 
-    private Weapon newWeapon(){
+    private Weapon newWeapon() {
+        //Almacenamos el poder y el numero de uses del nuevo arma
+        float power = Dice.weaponPower();
+        int uses = Dice.usesLeft();
 
+        return new Weapon(power, uses);
     }
 
-    private Shield newShield(){
+    private Shield newShield() {
+        //Almacenamos la proteccion y el numero de uses del nuevo escudo
+        float protection = Dice.shieldPower();
+        int uses = Dice.usesLeft();
 
+        return new Shield(protection, uses);
     }
 
-    private float sumWeapons(){
+    private float sumWeapons() {
+        float sumWeapons = 0;
 
+        // Iterar sobre la lista de armas y sumar el poder de cada arma
+        for (Weapon weapon : weapons) {
+            sumWeapons += weapon.attack();
+        }
+        return sumWeapons;
     }
 
-    private float sumShields(){
+    private float sumShields() {
+        float sumShields = 0;
 
+        // Iterar sobre la lista de armas y sumar el poder de cada arma
+        for (Shield shield : shields) {
+            sumShields += shield.protect();
+        }
+        return sumShields;
     }
 
-    private float defensiveEnergy(){
+    private float defensiveEnergy() {
 
+        return intelligence + sumShields();
     }
 
-    private boolean manageHit(float receivedAttack){
-
+    private boolean manageHit(float receivedAttack) {
+        throw new UnsupportedOperationException();
     }
 
-    private void resetHits(){
-
+    private void resetHits() {
+        consecutiveHits = 0;
     }
 
-    private void gotWounded(){
-
+    private void gotWounded() {
+        health--;
     }
 
-    private void incConsecutiveHits(){
-
+    private void incConsecutiveHits() {
+        consecutiveHits++;
     }
 
 }
